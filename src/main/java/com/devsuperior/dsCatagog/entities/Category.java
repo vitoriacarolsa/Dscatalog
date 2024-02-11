c782b033a322f786770d0f0df3d74668e13ff0fc
visantos <visantos@brasilseg.com.br>
2024-02-11 00:29:13 -0300

    Dados de auditoria

--- src/main/java/com/devsuperior/dsCatagog/entities/Category.java
+++ src/main/java/com/devsuperior/dsCatagog/entities/Category.java
@@ -2,6 +2,7 @@ package com.devsuperior.dsCatagog.entities;
 
 import jakarta.persistence.*;
 
+import java.time.Instant;
 import java.util.Objects;
 
 @Entity
@@ -13,6 +14,11 @@ public class Category {
     private Long id;
     private String name;
 
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
     public Category(){
     }
 
@@ -35,6 +41,24 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PrePersist
+    public void prePersist (){
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        updatedAt = Instant.now();
     }
 
     @Override
