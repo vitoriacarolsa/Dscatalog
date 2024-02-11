cecfcecbf87af5bc568e9d61da66a57368ff295f
visantos <visantos@brasilseg.com.br>
2024-02-11 03:14:01 -0300

    refatoração pageable

--- src/main/java/com/devsuperior/dsCatagog/resources/CategoryResource.java
+++ src/main/java/com/devsuperior/dsCatagog/resources/CategoryResource.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dsCatagog.resources;
 
 import com.devsuperior.dsCatagog.dto.CategoryDTO;
+import com.devsuperior.dsCatagog.dto.ProductDTO;
 import com.devsuperior.dsCatagog.entities.Category;
 import com.devsuperior.dsCatagog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,16 +23,10 @@ public class CategoryResource {
     @Autowired
     private CategoryService service;
     @GetMapping
-    public ResponseEntity<Page<CategoryDTO>> findAll(
-            @RequestParam(value= "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "linesPerpage", defaultValue = "12") Integer linesPerpage,
-            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
-            @RequestParam(value= "direction", defaultValue = "ASC") String direction){
-        PageRequest pageRequest = PageRequest.of(page, linesPerpage, Sort.Direction.valueOf(direction), orderBy);
-
-        Page<CategoryDTO>list= service.findAllPaged(pageRequest);
+    public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable){
+        Page<CategoryDTO>list= service.findAllPaged(pageable);
         return ResponseEntity.ok().body(list);
-  }
+    }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
--- src/main/java/com/devsuperior/dsCatagog/resources/ProductResource.java
+++ src/main/java/com/devsuperior/dsCatagog/resources/ProductResource.java
@@ -5,6 +5,7 @@ import com.devsuperior.dsCatagog.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,14 +19,9 @@ public class ProductResource {
     @Autowired
     private ProductService service;
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll(
-            @RequestParam(value= "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "linesPerpage", defaultValue = "12") Integer linesPerpage,
-            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
-            @RequestParam(value= "direction", defaultValue = "ASC") String direction){
-        PageRequest pageRequest = PageRequest.of(page, linesPerpage, Sort.Direction.valueOf(direction), orderBy);
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable){
 
-        Page<ProductDTO>list= service.findAllPaged(pageRequest);
+        Page<ProductDTO>list= service.findAllPaged(pageable);
         return ResponseEntity.ok().body(list);
   }
 
