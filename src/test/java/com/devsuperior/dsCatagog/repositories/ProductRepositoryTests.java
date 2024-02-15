1fcd42b852b3b03e47b44f962fd2c72d73a21214
visantos <visantos@brasilseg.com.br>
2024-02-14 22:24:50 -0300

    ProductRepositoryTests

---
+++ src/test/java/com/devsuperior/dsCatagog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,42 @@
+package com.devsuperior.dsCatagog.repositories;
+
+import com.devsuperior.dsCatagog.entities.Product;
+import com.devsuperior.dsCatagog.tests.Factory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+    @DataJpaTest
+    public class ProductRepositoryTests {
+
+        @Autowired
+        private ProductRepository productRepository;
+
+        private long exintingId;
+        private long countTotalProducts;
+        @BeforeEach
+        void setUp()throws Exception{
+            exintingId=1L;
+            countTotalProducts=25L;
+        }
+
+        @Test
+        public void deleteShouldDeleteObjectWhenIdExists(){
+            productRepository.deleteById(exintingId);
+            Optional<Product> result= productRepository.findById(exintingId);
+            Assertions.assertFalse(result.isPresent());
+        }
+
+        @Test
+        public void saveShouldPersistWithAutoincrementWhenIdIsNull(){
+            Product product = Factory.createProduct();
+            product.setId(null);
+
+            product = productRepository.save(product);
+            Assertions.assertNotNull(product.getId());
+            Assertions.assertEquals(countTotalProducts+1, product.getId());
+        }
+}
---
+++ src/test/java/com/devsuperior/dsCatagog/tests/Factory.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dsCatagog.tests;
+
+import com.devsuperior.dsCatagog.dto.ProductDTO;
+import com.devsuperior.dsCatagog.entities.Category;
+import com.devsuperior.dsCatagog.entities.Product;
+
+import java.time.Instant;
+
+public class Factory {
+    public static Product createProduct(){
+        Product product = new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2020-10-20T03:00:00Z"));
+        product.getCategories().add(new Category(2L, "Eletronics"));
+        return product;
+    }
+
+    public static ProductDTO createProductDTO(){
+        Product product = createProduct();
+        return new ProductDTO(product, product.getCategories());
+    }
+}
