98a4b8ac91f6aeb34262762cdcd97a3b20a70b68
vitoriacarolsa <vitoriacarolinesantos338@gmail.com>
2024-03-24 19:43:32 -0300

    Tratando exceção MethodArgumentNotValidException

--- src/main/java/com/devsuperior/dsCatagog/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/devsuperior/dsCatagog/resources/exceptions/ResourceExceptionHandler.java
@@ -5,6 +5,7 @@ import com.devsuperior.dsCatagog.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -31,6 +32,18 @@ public class ResourceExceptionHandler {
   err.setTimestamp(Instant.now());
   err.setStatus(status.value());
   err.setError("Database exception");
+  err.setMessage(e.getMessage());
+  err.setPath(request.getRequestURI());
+  return ResponseEntity.status(status.value()).body(err);
+ }
+
+ @ExceptionHandler(MethodArgumentNotValidException.class)
+ public ResponseEntity<StandarError> validation (MethodArgumentNotValidException e, HttpServletRequest request){
+  HttpStatus status= HttpStatus.UNPROCESSABLE_ENTITY;
+  StandarError err = new StandarError();
+  err.setTimestamp(Instant.now());
+  err.setStatus(status.value());
+  err.setError("Validation exception");
   err.setMessage(e.getMessage());
   err.setPath(request.getRequestURI());
   return ResponseEntity.status(status.value()).body(err);
