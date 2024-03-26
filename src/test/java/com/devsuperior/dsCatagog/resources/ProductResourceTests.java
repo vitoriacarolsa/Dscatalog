4d25fadeb7e28270d19ca960c15b8ed5db4199de
vitoriacarolsa <vitoriacarolinesantos338@gmail.com>
2024-03-26 03:34:01 -0300

    fazendo os testes de camada web passarem

--- src/test/java/com/devsuperior/dsCatagog/resources/ProductResourceTests.java
+++ src/test/java/com/devsuperior/dsCatagog/resources/ProductResourceTests.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
@@ -25,7 +26,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(ProductResource.class)
+@WebMvcTest(value = ProductResource.class, excludeAutoConfiguration = {SecurityAutoConfiguration.class})
 public class ProductResourceTests {
     @Autowired
     private MockMvc mockMvc;
