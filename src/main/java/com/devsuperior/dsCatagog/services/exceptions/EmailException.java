09793d9182fd014d4d011854595b102bba5b1b34
vitoriacarolsa <vitoriacarolinesantos338@gmail.com>
2024-04-03 00:45:14 -0300

    preparando projeto para envio de email

---
+++ src/main/java/com/devsuperior/dsCatagog/services/exceptions/EmailException.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dsCatagog.services.exceptions;
+
+@SuppressWarnings("serial")
+public class EmailException extends RuntimeException {
+
+	public EmailException(String msg) {
+		super(msg);
+	}
+}
